95d74e956b064be64b92c44d90b6f853342796e8
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2021-12-17 22:05:18 +0100

    azertyuiop
    azert

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const fs = require('fs')
 var MarkdownIt = require('markdown-it'), md = new MarkdownIt();
 function parse(file, tofile){
     fs.writeFileSync(tofile ,"<script src=\"https:\/\/cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/latest.js?config=AM_CHTML\"></script>" +  md.render(fs.readFileSync(file, 'utf-8')));
-
 }
+
 parse("cours2.md", "cours.html")
